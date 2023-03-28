594e74afa45ad01d832b41a5c750b759b8fbac5e
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-28 14:09:39 -0700

    :construction:

---
+++ src/components/location-selector/LocationSelector.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const LocationSelector = () => {
+    return (
+        <select>
+            <option value="Rossland">Rossland</option>
+            <option value="Trail">Trail</option>
+            <option value="Castlegar">Castlegar</option>
+        </select>
+    )
+}
+
+export default LocationSelector;
