ccdc2f83ee2d586a2722faf8cec84c141d558455
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-28 14:09:22 -0700

    :construction:

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,18 @@
 import React, { useState } from "react";
 import "./Header.css"
+import LocationSelector from "../location-selector/LocationSelector"
+import logo from "../../assets/dirtsurfer-logo.svg"
+
+const Header = () => {
+    return (
+        <header>
+            <a href="/">
+                <img src={logo} alt="Dirt Surfer logo" />
+            </a>
+            <LocationSelector />
+        </header>
+    )
+}
 
 
+export default Header;
