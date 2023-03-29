de31a53ac4e4fd056da358cea20fbd4457af17dc
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-28 19:46:47 -0700

    :construction:

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import "./Header.css"
 import logo from "../../assets/dirtsurfer-logo.svg"
+import SettingsButton from "../SettingsButton/SettingsButton"
+// import settingsIcon from "../../assets/settings-icon.svg"
 
 const Header = ({ onChange }) => {
     return (
-        <header>
+        <header className="header">
             <a href="/">
                 <img src={logo} alt="Dirt Surfer logo" />
             </a>
@@ -13,6 +15,7 @@ const Header = ({ onChange }) => {
                 <option value="Trail">Trail</option>
                 <option value="Castlegar">Castlegar</option>
             </select>
+            <SettingsButton />
         </header>
     )
 }
