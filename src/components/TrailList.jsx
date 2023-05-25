270b2a85334980d30fd3cdc87248773ef5b174e4
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-24 17:53:29 -0700

    :art: Prettier, and corrected source of starRating variable

--- src/components/TrailList.jsx
+++ src/components/TrailList.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Box, CircularProgress } from '@mui/material'
-import TrailCard from "./TrailCard";
+import React, { useEffect, useState } from "react"
+import { Box, CircularProgress } from "@mui/material"
+import TrailCard from "./TrailCard"
 
 export default function TrailList({ src }) {
     const [trailData, setTrailData] = useState([])
@@ -11,35 +11,43 @@ export default function TrailList({ src }) {
     useEffect(() => {
         setIsLoading(true)
         fetch(`http://127.0.0.1:6969/trails/${src}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        }})
-        .then((res) => res.json())
-        .then((data) => setTrailData(data))
-        .then(() => {
-            fetch(`http://127.0.0.1:6969/forecasts/${src}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                }})
-                .then((res) => res.json())
-                .then((data) => setForecastData(data))
-                .then(() => setIsLoading(false))
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
         })
+            .then((res) => res.json())
+            .then((data) => setTrailData(data))
+            .then(() => {
+                fetch(`http://127.0.0.1:6969/forecasts/${src}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                    .then((res) => res.json())
+                    .then((data) => setForecastData(data))
+                    .then(() => setIsLoading(false))
+            })
     }, [src])
 
     if (!isLoading) {
         // Run a sorting function on the objects first
         return trailData.map((trail, index) => {
             return (
-                <TrailCard key={index} trailName={trail.trailName} difficulty={trail.difficulty} starRating={trail.starRating} description={trail.description} descriptiveForecast={forecastData.find(({ trailName }) => trailName === `${trail.trailName
-                }`).descriptiveForecast} />
+                <TrailCard
+                    key={index}
+                    trailName={trail.trailName}
+                    difficulty={trail.difficulty}
+                    starRating={forecastData.find(({ trailName }) => trailName === `${trail.trailName}`).starRating}
+                    description={trail.description}
+                    descriptiveForecast={forecastData.find(({ trailName }) => trailName === `${trail.trailName}`).descriptiveForecast}
+                />
             )
         })
     } else {
         return (
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: "flex" }}>
                 <CircularProgress />
             </Box>
         )
